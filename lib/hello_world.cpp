9d81511f26169b64b8dff63fd2b23bc9fe5891cc
turcualexd <turcualex01@gmail.com>
2024-08-23 18:02:29 +0200

    Modified compiler tasks for Windows

--- lib/hello_world.cpp
+++ lib/hello_world.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+/*  <iostream> should not be avoided since it requires to statically include
+    big libraries to the executable, which results in very big programs (+2MB). */
 
 void hello_world() {
-    cout << "Hello world!\n";
+    printf("Hello world!\n");
 }
